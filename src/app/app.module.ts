78e3b6f9b030483a91a9ae30b4eb3ce4bd698b40
dsmriti <iamsmritid@gmail.com>
2020-11-07 20:49:07 +0530

    Removed used code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { ExportExcelService } from 'src/export-excel.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
